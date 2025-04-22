be18c338872cee494590165b7b385286f6f15ba6
varun sukalkar <92172651+varunsukalkar@users.noreply.github.com>
2025-04-22 21:04:08 +0530

    [35] [Time Beats: 0.72%] [Memory Beats: 100.00%] - LeetPush

---
+++ Search-Insert-Position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int high = nums.size()-1;
+      int   low =0;
+      while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high =mid-1;
+        }
+      }
+      return low;
+    }
+};
