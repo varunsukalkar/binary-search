fe20ed99ee421b8d6e0a0a355ce2afdf0a4b9bd1
varun sukalkar <92172651+varunsukalkar@users.noreply.github.com>
2025-04-22 22:14:29 +0530

    [540] [Time Beats: 4.05%] [Memory Beats: 100.00%] - LeetPush

---
+++ Single-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,39 @@
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        if(arr.size()==1){
+            return arr[0];
+        }
+        if(arr[0]!=arr[1]){
+            return arr[0];
+        }
+        if(arr[n-1]!=arr[n-2]){
+            return arr[n-1];
+        }
+
+        else{
+            int low = 1;
+            int high = arr.size()-2;
+            while(low<=high){
+                int mid=(low+high)/2;
+                if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]
+                ){
+                    return arr[mid];
+                }
+                if((mid%2==1 && arr[mid-1]==arr[mid])
+                || (mid%2 ==0 && arr[mid+1]==arr[mid])){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+
+
+            }
+        }
+        return -1;
+    }
+};
