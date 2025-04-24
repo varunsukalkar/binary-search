1c6de255f9fdd074f98d5b89b4504905ca4d7a8c
varun sukalkar <92172651+varunsukalkar@users.noreply.github.com>
2025-04-24 09:24:56 +0530

    [2799] [Time Beats: 92.24%] [Memory Beats: 59.44%] - LeetPush

---
+++ Count-Complete-Subarrays-in-an-Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+         int k = unordered_set<int>(nums.begin(),nums.end()).size();
+        unordered_map<int, int> mpp;
+             int left =0;
+             int res =0;
+        for (int i = 0; i < nums.size(); ++i) {
+            mpp[nums[i]]++;
+            while (mpp.size() == k) {
+                res += nums.size() - i;  
+                mpp[nums[left]]--;
+                if (mpp[nums[left]] == 0) mpp.erase(nums[left]);
+                left++;
+            }
+        }
+        return res;
+    
+
+    }
+};
